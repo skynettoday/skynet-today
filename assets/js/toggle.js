d91d0dea8bede4fdd346804fa560c3c00d4019a4
charlie <ckilpatrick6@gatech.edu>
2018-05-05 18:24:47 -0700

    Added newsletter to view toggle

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,24 +1,10 @@
-var displayingNews = true;
 $(function() {
-    var form = document.querySelector("#writingToggleForm");
-    form.addEventListener("change", function(event) {
+    var currentDisplay = $(".page-content > div:visible");
+    document.querySelector("#writingToggleForm").addEventListener("change", function(event) {
         console.log("Toggling writing display!");
-        if(displayingNews){
-          $('#news').fadeToggle(function() {
-              $('#editorials').fadeToggle(function() {
-              });
-          });
-        }else{
-          $('#editorials').fadeToggle(function() {
-              $('#news').fadeToggle(function() {
-              });
-          });
-        }
-        displayingNews = !displayingNews;
+        currentDisplay.fadeToggle(function() {
+            currentDisplay = $(event.srcElement.getAttribute("data-st-contentid"));
+            currentDisplay.fadeToggle();
+        });
     });
-    $("#editorials").fadeOut();
-
-    /*Reset selection*/
-    form.reset();
-});
-
+})
