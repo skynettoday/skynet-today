8f7bf84281390b6796da2a51843b9f56b6d59ca7
Andrey K <andrey@astroblox.ai>
2023-06-12 23:23:19 -0700

    Add logic for generating summaries, podcast, notes, and lwiai 223

--- scripts/csv2md.py
+++ scripts/csv2md.py
@@ -1,27 +1,18 @@
 import os
-import logging
 import argparse
 
 import pandas as pd
 import inflect
 import openai
 
+from newspaper import Article
 from pathlib import Path
 from datetime import date, timedelta
 from tqdm.auto import tqdm
 
 from tenacity import retry, stop_after_attempt, wait_random_exponential
 
-try:
-    import sys
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
-except:
-    print('Was not able to change sys encoding to utf-8, probably b/c you\'re on Python 3.')
-    pass
-
-
-_CATEGRORIES = [
+_CATEGORIES = [
     'Top News',
     'Research',
     'Applications',
@@ -53,12 +44,12 @@ Type:
     return response
 
 
-@retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
-def query_openai(messages):
+@retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(10))
+def query_openai(messages, max_tokens=10):
     return openai.ChatCompletion.create(
         model='gpt-3.5-turbo', 
         messages=messages,
-        max_tokens=10,
+        max_tokens=max_tokens,
         temperature=0
     ).choices[0]['message']['content']
 
@@ -91,6 +82,34 @@ Please only respond with one of the above types (Business, Resesarch, Applicatio
         {'role': 'user', 'content': prompt}
     ])
 
+def summarize_article(row):
+    article = Article(row['URL'])
+    try: 
+        article.download()
+        article.parse()
+        authors = article.authors
+        text = article.text
+        words = text.split(" ")
+        if len(words) > 2000:
+            text = " ".join(words[:2000])
+    except:
+        return ":("
+
+    system_prompt = '''
+Given the title, subtitle, and text of an article about AI, write a short (one or two sentence) summary of its content.
+DO NOT start with "The article" or "This article".
+'''.strip()
+    
+    prompt = f'''
+Title: {row['Name']}
+Subtitle: {row['Excerpt']}
+Text: {text}
+'''.strip()
+    return query_openai([
+        {'role': 'system', 'content': system_prompt},
+        {'role': 'user', 'content': prompt}
+    ],
+    max_tokens = 100)
 
 def get_article_type_manual(title, link, excerpt):
     print('To which category does this article belong?')
@@ -100,13 +119,13 @@ def get_article_type_manual(title, link, excerpt):
     print(row['Excerpt'].encode('utf-8'))
     print()
 
-    for i, c in enumerate(_CATEGRORIES):
+    for i, c in enumerate(_CATEGORIES):
         print(f'{i}) {c}')
     while True:
         try:
             print()
             c_idx = int(input('Category Number: '))
-            c = _CATEGRORIES[c_idx]
+            c = _CATEGORIES[c_idx]
             break
         except:
             print('Please enter a valid category!')
@@ -129,7 +148,6 @@ def get_output_file_name(n):
 
 
 if __name__ == "__main__":
-    logging.getLogger().setLevel(logging.INFO)
     parser = argparse.ArgumentParser()
     parser.add_argument('--template_file', '-tf', type=str, default='digest_template_website.md')
     parser.add_argument('--digest_number', '-n', type=int, required=True)
@@ -140,20 +158,20 @@ if __name__ == "__main__":
     n = args.digest_number
     p = inflect.engine()
     n_english = p.number_to_words(p.ordinal(n)).replace(' ', '-')
-    logging.info(f'Parsing for the {n_english} digest')
+    print(f'Parsing for the {n_english} digest')
 
     im_folder = Path(f'../assets/img/digests/{n}')
-    logging.info(f'Making image folder {im_folder}')
+    print(f'Making image folder {im_folder}')
     im_folder.mkdir(parents=True, exist_ok=True)
 
     output_md = Path('../_posts/digests') / get_output_file_name(n)
 
-    logging.info(f'Will save result to {output_md}')
+    print(f'Will save result to {output_md}')
     if os.path.isfile(output_md):
         if not args.force_overwrite:
             raise ValueError('Cannot overwrite existing output file!')
 
-    logging.info(f'Loading template from {args.template_file}')
+    print(f'Loading template from {args.template_file}')
     with open(args.template_file, 'r') as f:
         md_template = f.read()
 
@@ -164,35 +182,50 @@ if __name__ == "__main__":
     with open('secrets/openai_api_key.txt', 'r') as f:
         openai.api_key = f.read().strip()
 
-    logging.info(f'Reading {input_csv}')
-    articles_map = {c : [] for c in _CATEGRORIES}
+    print(f'Reading {input_csv}')
+    articles_map = {c : [] for c in _CATEGORIES}
     csv = pd.read_csv(input_csv, encoding='utf-8')
+    url_to_summary_map = {}
 
-    logging.info('Classifying articles...')
     rows_to_classify = {}
     for row_num, row in csv.iterrows():
+        if 'arxiv' in row['URL']:
+            continue
         has_type = 'Type' not in row or not row['Type'] or row['Type'] not in articles_map
         has_content = row['Name'] and row['Excerpt']
         if has_type and has_content:
             rows_to_classify[row_num] = row
+    
+    print('Classifying articles...')
     article_types = [classify_article_type(row) for row in tqdm(rows_to_classify.values())]
     article_types_map = {row_num: article_type for row_num, article_type in zip(rows_to_classify.keys(), article_types)}
-
+    
     for row_num, row in csv.iterrows():
+        if 'arxiv' in row['URL']:
+            continue
         if row_num in article_types_map:
             c = article_types_map[row_num]
-            if c not in _CATEGRORIES:
+            if c not in _CATEGORIES:
                 print(f'Row {row_num} classified as:', c, 'which is not a valid category!')
                 c = get_article_type_manual(row['Name'], row['URL'], row['Excerpt'])
         else:
             c = row['Type']
-
         articles_map[c].append(row)
+            
+    
+    print('Summarizing articles...')
+    for row_num, row in tqdm(rows_to_classify.items()):
+        summary = summarize_article(row)
+        url_to_summary_map[row['URL']] = summary
+        
+        print()
+        print(row['Name'])
+        print(summary)
 
-    logging.info('Populating content...')
+    print('Populating content...')
     top_news = ''
     content = ''
-    for c in _CATEGRORIES:
+    for c in _CATEGORIES:
         items = articles_map[c]
         if len(items) > 0:
             if c == 'Top News':
@@ -209,8 +242,8 @@ if __name__ == "__main__":
                 content += f'#### {c}'
                 content += '\n\n'
                 for item in items:
-                    name, url, excerpt = item['Name'], item['URL'], item['Excerpt']
-                    content += f'[{name}]({url}) - "{excerpt}"'
+                    name, url, summary = item['Name'], item['URL'], url_to_summary_map[item['URL']]
+                    content += f'[{name}]({url}) - "{summary}"'
                     content += '\n\n'
 
     # remove the last two empty lines
@@ -221,8 +254,8 @@ if __name__ == "__main__":
                     .replace('$top_news$', top_news) \
                     .replace('$content$', content)
 
-    logging.info('Saving digest markdown...')
+    print('Saving digest markdown...')
     with open(output_md, 'wb') as f:
         f.write(md.encode('utf-8'))
 
-    logging.info('Done!')
+    print('Done!')
---
+++ scripts/csv2podcast_notes.py
@@ -0,0 +1,198 @@
+import os
+import logging
+import argparse
+
+import pandas as pd
+import inflect
+import openai
+
+from newspaper import Article
+from pathlib import Path
+from datetime import date, timedelta
+from tqdm.auto import tqdm
+
+from tenacity import retry, stop_after_attempt, wait_random_exponential
+
+
+_CATEGORIES = [
+    'Tools & Apps',
+    'Applications & Business',
+    'Projects & Open Source',
+    'Research & Advancements',
+    'Policy & Safety',
+    'Synthetic Media & Art'
+]
+
+MAIN_STORY_COL = "Podcast pick (main story)"
+LIGHTING_STORY_COL = "Podcast pick (lighting round)"
+
+@retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
+def query_openai(messages):
+    return openai.ChatCompletion.create(
+        model='gpt-3.5-turbo', 
+        messages=messages,
+        max_tokens=1000,
+        temperature=0
+    ).choices[0]['message']['content']
+
+
+def classify_article_type(row):
+    prompt = f'''
+Title: {row['Name']}
+Description: {row['Excerpt']}
+Link: {row['URL']}
+'''.strip()
+
+    system_prompt = '''
+Your task is to classify articles about AI into one of the following types:
+Tools & Apps: Tools or applications that regular people can use for various purposes.
+Applications & Business: Applying AI to do something or anything related to product announcements, investments, funding, VCs, company updates, or market trends.
+Projects & Open Source: Misc projects that are not done by companies or academic labs, especially open source projects.
+Research & Advancements: Scientific studies, research in AI, or new results that advance IA.
+Policy & Safety: News, analysis, and opinions related to government policies or about AI risk/harm.
+Synthetic Media & Art: Things related to generative AI, deepfakes, or art and are not a better fit for the above categories.
+
+The user will provide the article title, link, and description. 
+After careful consideration, you will respond with ONLY the predicted article type, with no explanations, punctuation, formatting, or anything else.
+Please only respond with one of the above types (Tools & Apps, Applications & Business, Projects & Open Source,  Research & Advancements, Policy & Safety, Synthetic Media & Art).
+'''.strip()
+    return query_openai([
+        {'role': 'system', 'content': system_prompt},
+        {'role': 'user', 'content': prompt}
+    ])
+
+def summarize_article(row, lighting_round_story=False):
+    article = Article(row['URL'])
+    try: 
+        article.download()
+        article.parse()
+        authors = article.authors
+        text = article.text
+        words = text.split(" ")
+        if len(words) > 2000:
+            text = " ".join(words[:2000])
+    except:
+        return ":("
+
+    system_prompt = '''
+Your task is to provide a bullet point summary of a news article about AI. Each bullet point should be no more than 2 sentences long. This summary will be used for the podcast Last Week in AI, in which the hosts summarize stories about AI in an accessible manner. We will provide the title of the article, the subtitle, and the text. Output the bullet points in markdown format.'''
+
+    if lighting_round_story:
+        system_prompt + " This story will be in a lighting round, so summarize it in no more than 8 bullet points, but still make sure to cover all the important details."
+    else:
+        system_prompt + " This will be covered as a main story, so produce a detailed summary with as many as 15 bullet points."
+        
+    system_prompt = system_prompt.strip()
+    
+    prompt = f'''
+Title: {row['Name']}
+Subtitle: {row['Excerpt']}
+Text: {text}
+'''.strip()
+    return query_openai([
+        {'role': 'system', 'content': system_prompt},
+        {'role': 'user', 'content': prompt}
+    ])
+
+
+def get_article_type_manual(title, link, excerpt):
+    print('To which category does this article belong?')
+    print()
+    print(row['Name'].encode('utf-8'))
+    print()
+    print(row['Excerpt'].encode('utf-8'))
+    print()
+
+    for i, c in enumerate(_CATEGRORIES):
+        print(f'{i}) {c}')
+    while True:
+        try:
+            print()
+            c_idx = int(input('Category Number: '))
+            c = _CATEGRORIES[c_idx]
+            break
+        except:
+            print('Please enter a valid category!')
+    print()
+
+    return c
+
+
+
+if __name__ == "__main__":
+    with open('secrets/openai_api_key.txt', 'r') as f:
+        openai.api_key = f.read().strip()
+
+    articles_map = {c : ([],[]) for c in _CATEGORIES}
+    articles_map['other'] = ([],[])
+    with open("news.csv", 'r') as f:
+        num_picks = f.read().count(",X")
+    csv = pd.read_csv('news.csv', encoding='utf-8')
+    
+    pbar = tqdm(total=num_picks)
+    count = 0
+    for row_num, row in tqdm(csv.iterrows()):
+        has_content = row['Name'] and row['Excerpt']
+        is_main_pick = row[MAIN_STORY_COL]=='X'
+        is_lighting_pick = row[LIGHTING_STORY_COL]=='X'
+        if not has_content or not (is_main_pick or is_lighting_pick):
+            continue
+        category = classify_article_type(row)
+        print('\nProcessing "%s"'%row['Name'])
+        if is_main_pick:
+            print("Type: Main story")
+        else:
+            print("Type: Lighting round story")
+        print("Link: %s"%row['URL'])
+        print("Category: %s"%category)
+        if category not in _CATEGORIES:
+            category = 'other'
+        summary = summarize_article(row,is_lighting_pick)
+        print('Summary:')
+        print(summary)
+        if is_main_pick:
+            articles_map[category][0].append([row['Name'],row['URL'],summary])
+        else:
+            articles_map[category][1].append([row['Name'],row['URL'],summary])
+        print("")
+        count+=1
+        pbar.update(1)
+    pbar.close()
+        
+    content = 'Outline:\n'
+    
+    for c in _CATEGORIES:
+        content+="- "+c+"\n"
+        items = articles_map[c]
+        for item in items[0]:        
+            name, url, summary = item
+            content += f'   - [{name}]({url})\n'
+        content+="  - Lighting round\n"
+        for item in items[1]: 
+            name, url, summary = item       
+            content += f'       - [{name}]({url})\n'
+            
+    content +="\n\n#Summaries\n\n"
+    for c in _CATEGORIES:
+        items = articles_map[c]
+        content += f'## {c}'
+        if len(items[0]) > 0:
+            content += '\n\n'
+            for item in items[0]:
+                name, url, summary = item
+                content += f'[{name}]({url})'
+                content += '\n'
+                content += summary
+                content += "\n\n"
+        content +="\n###Lighting Round\n\n"
+        if len(items[1]) > 0:
+            for item in items[1]:
+                name, url, summary = item
+                content += f'[{name}]({url})'
+                content += '\n'
+                content += summary
+                content += "\n\n"
+                
+    print(content)
+    with open("summaries.md", 'wb') as f:
+        f.write(content.encode('utf-8'))
