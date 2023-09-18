a68ce0660498f238447e01efc84aa1413ddbcd5e
Jacky Liang <jackyliang42@gmail.com>
2023-09-17 23:35:35 -0700

    digest_237

--- scripts/gen_newsletter.sh
+++ scripts/gen_newsletter.sh
@@ -13,17 +13,17 @@ python csv2md.py -n=$digest_num
 
 # Push changes
 
-rm "*news.csv"
+rm *.csv
 
 git commit -am $digest_name
-git push --set-upstream origin $digest_name
+# git push --set-upstream origin $digest_name
 
-gh pr create --title "Digest $digest_num" --body "Digest $digest_num" --base master --head $digest_name --label "digest"
-gh pr merge --auto --merge --delete-branch "$digest_name"
+# gh pr create --title "Digest $digest_num" --body "Digest $digest_num" --base master --head $digest_name --label "digest"
+# gh pr merge --auto --merge --delete-branch "$digest_name"
 
-git checkout master
-git pull
+# git checkout master
+# git pull
 
 # Generate socials
 
-python md2socials.py -n=$digest_num
+# python md2socials.py -n=$digest_num
