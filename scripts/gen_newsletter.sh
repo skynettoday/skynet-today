c0047f5ee405d379e380731240e6b5cd709097c4
Jacky Liang <jackyliang42@gmail.com>
2023-10-16 00:07:24 -0700

    digest_241

--- scripts/gen_newsletter.sh
+++ scripts/gen_newsletter.sh
@@ -5,26 +5,19 @@ digest_name=digest_$digest_num
 
 # Make digest
 
-git checkout master
-git pull
-git checkout -b digest_$digest_num
-
+git checkout master; git pull; git checkout -b digest_$digest_num
 python csv2md.py -n=$digest_num
 
 # Push changes
 
 rm *.csv
-
-git add ../_posts/digests/*
-git add ../assets/img/digests/$digest_num/*
-git commit -am $digest_name
-git push --set-upstream origin $digest_name
+git add ../_posts/digests/* ../assets/img/digests/$digest_num/*
+git commit -am $digest_name; git push --set-upstream origin $digest_name
 
 gh pr create --title "Digest $digest_num" --body "Digest $digest_num" --base master --head $digest_name --label "digest"
 gh pr merge --auto --merge --delete-branch "$digest_name"
 
-git checkout master
-git pull
+git checkout master; git pull
 
 # Generate socials
 
