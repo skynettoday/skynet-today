57d4520e11a28691e7cc5b42733be60efced05c6
andreykurenkov <andrey@astroblox.ai>
2023-12-29 10:23:00 -0500

    Updating 2023 summary

--- scripts/gen_year_end_post.py
+++ scripts/gen_year_end_post.py
@@ -4,15 +4,43 @@ import requests
 from PIL import Image
 from io import BytesIO
 from collections import defaultdict
+from bs4 import BeautifulSoup
+import math 
+from statistics import mean
 
-def extract_top_news_and_images(content):
+def extract_top_news(content):
+    """
+    Extracts individual articles from the 'Top News' section in the markdown content.
+    The format is:
+        "#### [Title](link)"
+    """
     top_news_pattern = r'### Top News\n\n(.*?)(\n### |\Z)'
     top_news_match = re.search(top_news_pattern, content, re.DOTALL)
     if top_news_match:
         top_news_section = top_news_match.group(1).strip()
-        articles_and_images = re.findall(r'#### \[(.*?)\]\((.*?)\)\s*!\[.*?\]\((.*?)\)', top_news_section)
-        return articles_and_images
+        articles = re.findall(r'#### \[(.*?)\]\((.*?)\)', top_news_section)
+        return articles
     return []
+
+def fetch_image_urls(url):
+    images = []
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            divs = soup.find_all("div", class_="captioned-image-container")
+            for div in divs:
+                figure = div.find("figure")
+                if figure:
+                    a_tag = figure.find("a", class_="image-link")
+                    if a_tag and 'href' in a_tag.attrs:
+                        image_url = a_tag['href']
+                        images.append(image_url)
+        else:
+            print(f"Failed to fetch the website: {url}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return images
 
 def month_name(month_number):
     months = ["January", "February", "March", "April", "May", "June",
@@ -32,14 +60,15 @@ def download_and_resize_images(image_urls, max_width, max_height):
     return images
 
 def create_collage(images, output_path):
-    rows = (len(images) + 2) // 3  # Arrange up to 3 images per row
-    cols = min(len(images), 3)
+    max_per_row = int(math.sqrt(len(images)))
+    rows = len(images) // max_per_row  # Arrange up to max_per_row images per row
+    cols = min(len(images), max_per_row)
 
     # Correctly calculate the shortest height in each row
-    heights_per_row = [min(img.size[1] for img in images[i * cols:(i + 1) * cols]) for i in range(rows)]
+    heights_per_row = [int(mean(img.size[1] for img in images[i * cols:(i + 1) * cols])) for i in range(rows)]
     total_height = sum(heights_per_row)
     max_width = max(img.size[0] for img in images) * cols
-
+ 
     collage = Image.new('RGB', (max_width, total_height), (255, 255, 255))  # RGB with white background
 
     y_offset = 0
@@ -57,10 +86,8 @@ def create_collage(images, output_path):
     collage.save(output_path, "JPEG")
     print(f"Collage image saved at {output_path}")
 
-
 def parse_markdown_files(directory):
     news_by_month = defaultdict(list)
-    images_by_month = defaultdict(list)
     for filename in os.listdir(directory):
         if filename.endswith('.md') and filename.startswith('2023'):
             file_path = os.path.join(directory, filename)
@@ -70,13 +97,12 @@ def parse_markdown_files(directory):
             if date_match:
                 month = date_match.group(1)
                 week_number = date_match.group(2)
-                articles_and_images = extract_top_news_and_images(content)
-                if articles_and_images:
-                    news_by_month[month].append((week_number, [item[:2] for item in articles_and_images]))
-                    images_by_month[month].extend([item[2] for item in articles_and_images])
-    return news_by_month, images_by_month
+                articles = extract_top_news(content)
+                if articles:
+                    news_by_month[month].append((week_number, articles))
+    return news_by_month
 
-def generate_summary_markdown(news_by_month, images_by_month, output_file_path, img_directory):
+def generate_summary_markdown(news_by_month, output_file_path, img_directory):
     with open(output_file_path, 'w') as file:        
         file.write("---\nlayout: post\ntitle: \"AI News in 2023: a Digest\"\n")
         file.write("excerpt: \"An overview of the big AI-related stories from 2023\"\n---\n")
@@ -85,8 +111,12 @@ def generate_summary_markdown(news_by_month, images_by_month, output_file_path, 
         for month, news_items in sorted(news_by_month.items()):
             month_name_str = month_name(month)
             print(month_name_str)
-            print(news_items)
-            image_urls = images_by_month[month]
+            for news_week in news_items:
+                for news_item in news_week[1]:
+                    print('\t' + news_item[0])
+            image_urls = []
+            for week_number, _ in sorted(news_items, key=lambda x: int(x[0])):
+                image_urls+=fetch_image_urls(f'https://lastweekin.ai/p/{week_number}')
             resized_images = download_and_resize_images(image_urls, 300, 300)  # Resize images to a max of 300x300
             collage_image_path = f"{img_directory}/collage_{month_name_str}.jpg"
             create_collage(resized_images, collage_image_path)
@@ -111,12 +141,12 @@ def generate_summary_markdown(news_by_month, images_by_month, output_file_path, 
     return output_file_path
 
 def main(directory, output_file_path, img_directory):
-    news_by_month, images_by_month = parse_markdown_files(directory)
-    summary_file_path = generate_summary_markdown(news_by_month, images_by_month, output_file_path, img_directory)
+    news_by_month = parse_markdown_files(directory)
+    summary_file_path = generate_summary_markdown(news_by_month, output_file_path, img_directory)
     print(f"Summary markdown file generated at: {summary_file_path}")
 
 
 # To use this script, specify the directory of markdown files and the output path for the summary file.
 # For example:
 # main('/path/to/markdown/files', '/path/to/output/2023-summary.md')
-main("_posts/digests","_posts/overviews/2023-12-23-2023-summary.md", "assets/img/overviews/2023-12-23-2023-summary")
+main("_posts/digests","_posts/overviews/2023-12-30-2023-summary.md", "assets/img/overviews/2023-12-30-2023-summary")
